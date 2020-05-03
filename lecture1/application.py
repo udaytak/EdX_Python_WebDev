a325e4dd5a8693b302e760145e55f66c056378df
Uday Tak <tak.uday@gmail.com>
2020-05-03 12:44:51 -0700

    flask multipage website

--- lecture1/application.py
+++ lecture1/application.py
@@ -3,6 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+def home():
+    return "Hello, World !"
 
-def index():
-    return "Hello, World!!"
+@app.route("/david")
+def david():
+    return "Hello, David !"
+
+if __name__ == "__main__":
+    app.run(debug=True)
