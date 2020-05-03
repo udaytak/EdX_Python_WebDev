8802f8c24648530a2ca0626ff8b0ca7980459b97
Uday Tak <tak.uday@gmail.com>
2020-05-03 13:56:40 -0700

    flask html variable passing

--- lecture1/app1.py
+++ lecture1/app1.py
@@ -4,4 +4,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    headline = "Hello World!"
+    return render_template("index.html", headline=headline)
